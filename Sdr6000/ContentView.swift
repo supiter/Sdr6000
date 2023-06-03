93a9bcb16ac446a38c203a35513783a7fb699dec
Douglas Adams <douglas.adams@me.com>
2023-06-03 11:19:53 -0400

    Initial Commit

---
+++ Sdr6000/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Sdr6000
+//
+//  Created by Douglas Adams on 6/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sdr6000/Sdr6000App.swift
@@ -0,0 +1,17 @@
+//
+//  Sdr6000App.swift
+//  Sdr6000
+//
+//  Created by Douglas Adams on 6/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Sdr6000App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
